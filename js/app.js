ccb799e951dfe891ac35b77b5fdd5f1ca0361421
Michael-Rolland <michael@devoji.com>
2023-07-13 11:34:24 +0200

    form

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.log("test");
+
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêche le formulaire de se soumettre
+
+  const input = document.getElementById('name').value;
+  const message = `Salut ${input} !\nTu peux m'écrire à michaël@devoji.com`;
+
+  alert(message);
+});
