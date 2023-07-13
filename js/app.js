7ebec80919153ea5387087ee2643eba78eb2d93a
Michael-Rolland <michael@devoji.com>
2023-07-13 12:10:22 +0200

    blocage charg

--- js/app.js
+++ js/app.js
@@ -5,8 +5,13 @@ form.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the form from submitting
 
   const input = document.getElementById('name').value;
+  
+  if (input.toLowerCase() === "chargueraud") {
+    alert("Le nom 'Chargueraud' n'est pas autorisé.");
+    return;
+  }
+  
   const message = `Salut ${input}!<br><br>Tu peux m'écrire à michael@devoji.com.<br>Tu peux également visiter le site <a href="https://www.devoji.com">www.devoji.com</a>.`;
-
 
   const customAlert = document.createElement('div');
   customAlert.classList.add('custom-alert');
@@ -16,5 +21,5 @@ form.addEventListener('submit', function(event) {
 
   setTimeout(() => {
     customAlert.remove();
-  }, 10000);
+  }, 3000);
 });
